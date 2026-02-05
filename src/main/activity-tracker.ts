2c48849a8086b3c885f31d194e8ddba61414106a
Dhruvi <dhruvipatel0268@gmail.com>
2026-02-05 11:38:09 +0530

    Updated

--- src/main/activity-tracker.ts
+++ src/main/activity-tracker.ts
@@ -26,7 +26,7 @@ export class ActivityTracker extends EventEmitter {
     } = {};
 
     private lastScreenshotTime: number = Date.now();
-    private screenshotIntervalMs: number = 5 * 60 * 1000; // 1 minute
+    private screenshotIntervalMs: number = 5 * 60 * 1000; // 5 minute
 
     constructor() {
         super();
